6637e1fd27b6493a3895aca7b81f08e23b94362b
Ontida Tanthmanatham <ontidaxt@gmail.com>
2016-04-07 16:48:30 -0700

    Completed all the exercises except the extra credit in the koans/AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +41,15 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      _.any((_.filter(products,function(product){
+          return product.containsNuts === false;
+        })),function(selected){
+          if(selected.ingredients.indexOf("mushrooms") === -1){
+          productsICanEat.push(selected.name);
+        }
+      });
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +63,21 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.reduce(_.range(1,1000), function(prev,current){
+      if(current % 3 === 0 || current % 5 === 0){
+        return prev + current;
+      }
+      else return prev
+    },0);
+    /* try chaining range() and reduce() */
+    
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,7 +90,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
@@ -83,7 +98,19 @@ describe("About Applying What We Have Learnt", function() {
 
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    var countResult = _(products).chain()
+                        .map(function(product){
+                          return product.ingredients;
+                        })
+                        .flatten()
+                        .reduce(function(prev, current){
+                            if (ingredientCount[current] === undefined){
+                              return ingredientCount[current] = 1;
+                            } else {return ingredientCount[current] += 1};         
+                        },0)
+                        .value();
+
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
@@ -109,5 +136,6 @@ describe("About Applying What We Have Learnt", function() {
   it("should find the 10001st prime", function () {
 
   });
-  */
+
+*/  
 });
